bf1d38ba152623c3b9b085b554d2d18e20dfeb29
khanhhongtranle <khanhhong141049@gmail.com>
2021-04-27 19:01:20 +0700

    access my wallet

--- src/app/public-components/access-my-wallet/access-my-wallet.component.ts
+++ src/app/public-components/access-my-wallet/access-my-wallet.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-access-my-wallet',
-  templateUrl: './access-my-wallet.component.html',
-  styleUrls: ['./access-my-wallet.component.css']
+    selector: 'app-access-my-wallet',
+    templateUrl: './access-my-wallet.component.html',
 })
 export class AccessMyWalletComponent implements OnInit {
+    public privateKey: string;
+    public password: string;
 
-  constructor() { }
+    constructor() {
+        this.privateKey = '';
+        this.password = '';
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    public clickToAccessHandle(): void {
+        if (this.privateKey === '' || this.password === ''){
+            return;
+        }
+        // service login
+    }
 }
